8e24828f50892fd9d732e73505fd50fcbebcda81
muhammedshabeen <muhammedshabeen982@gmail.com>
2024-12-20 12:09:08 +0530

    project basic setup

---
+++ user_app/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password')}),
+        ('Personal Info', {'fields': ('name', 'gender', 'phone_number', 'address', 'd_o_b', 'image')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_staff', 'is_active')}
+        ),
+    )
+    search_fields = ('email', 'username')
+    ordering = ('email',)
+
+admin.site.register(CustomUser, CustomUserAdmin)
---
+++ user_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from core.managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):    
+    GENDER_CHOICES = [
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+        ('Other', 'Other'),
+    ]
+    USER_CHOICES = [
+        ('Admin', 'Admin'),
+        ('User', 'User'),
+    ]
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100,unique=True)
+    name = models.CharField(max_length=50)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=10)
+    d_o_b = models.DateField()
+    address = models.TextField()
+    gender = models.CharField(max_length=15,choices=GENDER_CHOICES)
+    user_type = models.CharField(max_length=15,choices=USER_CHOICES,default='Admin')
+    image = models.FileField(upload_to='profile')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = CustomUserManager()
